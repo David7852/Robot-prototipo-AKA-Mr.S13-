3096e83e1dde2d09c6389f48c85dc99285984bc6
David7852 <dyd785265@hotmail.com>
2016-02-06 20:27:49 -0430

    debug

--- main.asm
+++ main.asm
@@ -1,263 +1,262 @@
 ;Rutina de inicializacion de puertos y variables aqui. (FALTA POR AHORA)
 
-;del 00 al 1f son regs de proposito general, marcados como r0 a r31
+;del 00 al 1f son regs de proposito general
 ;del 20 al 5f (00 a 3f) estan los registros validos para I/O, sumando otros 32 regs
 ;.dseg
-;r0 = objeto (# casilla)
-;r1 = X objeto
-;r2 = Y objeto
-;r3 = aux 1
-;r4 = aux 2
-;r5 = aux 3
-;r6 = N (casilla en busqueda o N de la secuencia)
-;r7 = NDS (Numero de pasos para giro)
-;r8 = sector(00 A 01 B)
-;r9 = X actual
-;r10 = Y actual
-;r11 = sentido (abajo 00, arriba 01, derecha 02, izquierda 03,  con el eje ortocentrico en el sector A borde izquierdo)
-;r12-19 = aux
+;r16 = objeto (# casilla)
+;r17 = X objeto
+;r18= Y objeto
+;r19 = aux 1
+;r20 = aux 2
+;r21 = aux 3
+;r22 = N (casilla en busqueda o N de la secuencia)
+;r23 = NDS (Numero de pasos para giro)
+;r24 = sector(00 A 01 B)
+;r25 = X actual
+;r26 = Y actual
+;r27 = sentido (abajo 00, arriba 01, derecha 02, izquierda 03,  con el eje ortocentrico en el sector A borde izquierdo)
+;r28-19 = aux
 ;.......
-;r20 = I/O puerto C (sector A)
-;r21 = I/O puerto D (sector B)
-;r22 = I/O puerto F (Bordes / )
-;r23 = I/O puerto B (motores)
+;r2 = I/O puerto C (sector A)
+;r3 = I/O puerto D (sector B)
+;r4 = I/O puerto F (Bordes / )
+;r5 = I/O puerto B (motores)
 ;***  
 
 .cseg 
-.org 1000
+.org 0000
 
-start:
+start:;lee la posicion de objeto
 nop
-ldi r3,0;initial setup port
-out ddrd,r3
-out ddrf,r3
-out ddrc,r3
-ldi r3,0xff
-out ddrb,r3
-in r20, pinc
+ldi r19,0;initial setup port
+out ddrd,r19
+out ddrf,r19
+out ddrc,r19
+ldi r19,0xff
+out ddrb,r19
+in r2, pinc
 nop
-com r20
-in r21, pind
+com r2
+in r3, pind
 nop
-com r21;end of initial setup
-cp r20,r3
+com r3;end of initial setup
+cp r2,r19
 brne startA
-cpse r21,r3
+cpse r3,r19
 jmp startB
 jmp start
 
-startA:
+startA:;setea la posicion del objeto si esta en sector A
 nop
-SBRC r20,0
+SBRC r2,0
 jmp setstartA
-inc r3
-SBRC r20,1
+inc r19
+SBRC r2,1
 jmp setstartA
-inc r3
-SBRC r20,2
+inc r19
+SBRC r2,2
 jmp setstartA
-inc r3
-SBRC r20,3
+inc r19
+SBRC r2,3
 jmp setstartA
-inc r3
-SBRC r20,4
+inc r19
+SBRC r2,4
 jmp setstartA
-inc r3
-SBRC r20,5
+inc r19
+SBRC r2,5
 jmp setstartA
-inc r3
-SBRC r20,6
+inc r19
+SBRC r2,6
 jmp setstartA
-inc r3
+inc r19
 jmp setstartA
 
-startB:
+startB:;setea la posicion del objeto si esta en sector B
 nop
-SBRC r21,0
+SBRC r3,0
 jmp setstartB
-inc r3
-SBRC r21,1
+inc r19
+SBRC r3,1
 jmp setstartB
-inc r3
-SBRC r21,2
+inc r19
+SBRC r3,2
 jmp setstartB
-inc r3
-SBRC r21,3
+inc r19
+SBRC r3,3
 jmp setstartB
-inc r3
-SBRC r21,4
+inc r19
+SBRC r3,4
 jmp setstartB
-inc r3
-SBRC r21,5
+inc r19
+SBRC r3,5
 jmp setstartB
-inc r3
-SBRC r21,6
+inc r19
+SBRC r3,6
 jmp setstartB
-inc r3
+inc r19
 jmp setstartB
 
 setstartA:
 nop
-mov r0,r3
-mov r6,r0
+mov r16,r19
+mov r22,r16
 call getxy
-jmp deduindi
+mov r17,r20
+mov r18,r21
+jmp stindi;rutina de solucion de sector
 
 setstartB:
 nop
-ldi r4,8
-add r3,r4
-mov r0,r3
-mov r6,r0
+ldi r20,8
+add r19,r20
+mov r16,r19
+mov r22,r16
 call getxy
-mov r1,r4
-mov r2,r5
+mov r17,r20
+mov r18,r21
 jmp stindi
 ;fin de rutinas de deteccion de objeto
 
 ;obtener las coordenadas xy de una casilla
-;el valor de la casilla a buscar debe estar guardado en r6, el X y Y resultado se guardara en r4 y r5.
+;el valor de la casilla a buscar debe estar guardado en r22, el X y Y resultado se guardara en r20 y r21.
 getxy:
 nop
-ldi r3,0
-ldi r4,0
-ldi r5,0
+ldi r19,0
+ldi r20,0
+ldi r21,0
 rjmp subgetxy
 
 subgetxy:
 nop
-cpse r3,r6
+cpse r19,r22
 jmp loopxy
 ret
 loopxy:
 nop
-cpi r4,3
+cpi r20,3
 breq eqloopxy
-inc r4
-inc r3
+inc r20
+inc r19
 jmp subgetxy 
 eqloopxy:
 nop
-inc r5
-ldi r4,0
-inc r3
+inc r21
+ldi r20,0
+inc r19
 jmp subgetxy
 ;fin
 
 ;obtener el valor actual de una casilla N
-;la casilla a buscar debe estar guardada en r6, el valor (0,1) es devuelto en el registro 3
+;la casilla a buscar debe estar guardada en r22, el valor (0,1) es devuelto en el registro 3
 getval:
 nop
-ldi r3,0
-cpi r6,8
+ldi r19,0
+cpi r22,8
 brsh getvalB
 jmp getvalA
 
 getvalB:
 nop
-in r21,pind
+in r3,pind
 nop
-com r21
-ldi r4,8
-SBRC r21,0
-ldi r3,1
-cp r6,r4
+com r3
+ldi r20,8
+SBRC r3,0
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r21,1
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r3,1
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r21,2
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r3,2
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r21,3
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r3,3
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r21,4
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r3,4
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r21,5
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r3,5
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r21,6
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r3,6
+ldi r19,1
+cp r22,r20
 breq retgeval
-SBRC r21,7
-ldi r3,1
+SBRC r3,7
+ldi r19,1
 jmp retgeval
+
+retgeval:
+nop
+ret
 
 getvalA:
 nop
-in r20,pinc
+in r2,pinc
 nop
-com r20
-ldi r4,0
-SBRC r20,0
-ldi r3,1
-cp r6,r4
+com r2
+ldi r20,0
+SBRC r2,0
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r20,1
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r2,1
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r20,2
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r2,2
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r20,3
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r2,3
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r20,4
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r2,4
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r20,5
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r2,5
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r20,6
-ldi r3,1
-cp r6,r4
+inc r20
+SBRC r2,6
+ldi r19,1
+cp r22,r20
 breq retgeval
-inc r4
-SBRC r20,7
-ldi r3,1
+inc r20
+SBRC r2,7
+ldi r19,1
 jmp retgeval
 
-regeval
-nop
-ret
 ;fin
 
-;dadas las coordenadas xy guardadas en los registros r4 y r5 respectivamente, guarda el numero de esa casilla en r6
+;dadas las coordenadas xy guardadas en los registros r20 y r21 respectivamente, guarda el numero de esa casilla en r22
 getNxy:
 nop
-mov r12,r0
-mov r13,r1
-ldi r6,4
-mul r6,r5
-mov r6,r0
-mov r0,12
-mov r1,13
-add r6,r4
+ldi r22,4
+mul r22,r21
+mov r22,r0
+add r22,r20
 ret
 ;fin
 
@@ -267,19 +266,19 @@ ret
 ;004 (12(0.00000005))(256)(256)=0.039
 wait002:
 nop
-ldi r4,0xff
+ldi r20,0xff
 jmp wait02A
 
 wait02A:
 nop
-ldi r5,0xff
-subi r4,1
+ldi r21,0xff
+subi r20,1
 brne wait02B
 ret
 
 wait02B:
 nop
-subi r5,1
+subi r21,1
 nop
 nop
 nop
@@ -288,19 +287,19 @@ rjmp wait02B
 
 wait003:
 nop
-ldi r4,0xff
+ldi r20,0xff
 jmp wait03A
 
 wait03A:
 nop
-ldi r5,0xff
-subi r4,1
+ldi r21,0xff
+subi r20,1
 brne wait03B
 ret
 
 wait03B:
 nop
-subi r5,1
+subi r21,1
 nop
 nop
 nop
@@ -311,19 +310,19 @@ rjmp wait03B
 
 wait004:
 nop
-ldi r4,0xff
+ldi r20,0xff
 jmp wait04A
 
 wait04A:
 nop
-ldi r5,0xff
-subi r4,1
+ldi r21,0xff
+subi r20,1
 brne wait04B
 ret
 
 wait04B:
 nop
-subi r5,1
+subi r21,1
 nop
 nop
 nop
@@ -341,34 +340,34 @@ rjmp wait04B
 ;retrasa el carro 5.6 seg, 7,4 seg, 9,9 seg respectivamente
 esperanto:
 nop
-ldi r3,0xff
+ldi r19,0xff
 rjmp wait00
 
 wait00:
 nop
 call wait003;retraso de 0.029
-in r22,pinf
+in r4,pinf
 nop
-com r22 ;suponiendo que las entradas de los bordes sean logica baja al igual que los sensores
-SBRC r22,0 ;si el pin 0 NO es 0, salta, si lo es ignora
+com r4 ;suponiendo que las entradas de los bordes sean logica baja al igual que los sensores
+SBRC r4,0 ;si el pin 0 NO es 0, salta, si lo es ignora
 rjmp setsecA
-sbrc r22,1 ;si el pin 1 NO es 0, salta, si lo es ignora
+sbrc r4,1 ;si el pin 1 NO es 0, salta, si lo es ignora
 rjmp setsecB
-subi r3,1
+subi r19,1
 breq resolution
 rjmp wait00
 ;fin de rutina de espera
 
 setsecA:
 nop
-ldi r8,0
-ldi r11,0
+ldi r24,0
+ldi r27,0
 jmp deteccion
 
 setsecB:
 nop
-ldi r8,1
-ldi r11,1
+ldi r24,1
+ldi r27,1
 jmp deteccion
 
 ;deteccion de en que casilla estoy
@@ -387,163 +386,145 @@ nop
 
 
 
-
-
-
-
 ;deducir la proxima casilla a moder.
-deducir:
-nop
-mov r6,r0
-call getval
-cpi r3,0
-breq GOBACK
-jmp RutSel
 
-RutSel:
+backA:
+jmp moverdown
+in r4,pinf
 nop
-cp r2,r10
-brlo moverdown
-brne moverup
-cp r1,r9
-brlo moverleft
-brne moverright
-mov r6,r0
-call getval
-cpi r3,0
-breq GOBACK
-jmp moveradel
+com r4
+SBRS r4,0
+jmp backA
+jmp gostop
 
-moverup:
+backB:
+jmp moverup
+in r4,pinf
 nop
-inc r10
-cpi r11,0
-breq forward
-cpi r11,1
+com r4
+SBRS r4,1
+jmp backB
+jmp gostop
+
+turnleft:
+call pasoder
+jmp fordward
+
+moverleft:
+dec r25
+cpi r27,2
 breq reverse
-cpi r11,2
-ldi r11,0
-breq turnright
-jmp turnleft
-
-moverdown:
-nop
-dec r10
-cpi r11,0
-breq reverse
-cpi r11,1
-breq forward
-cpi r11,2
-ldi r11,1
+cpi r27,3
+breq fordward
+cpi r27,0
+ldi r27,2
 breq turnleft
 jmp turnright
 
 moverright:
-nop
-inc r9
-cpi r11,2
-breq forward
-cpi r11,3
+inc r25
+cpi r27,2
+breq fordward
+cpi r27,3
 breq reverse
-cpi r11,0
-ldi r11,2
+cpi r27,0
+ldi r27,2
 breq turnright
 jmp turnleft
 
-moverleft:
-nop
-dec r9
-cpi r11,2
-breq reverse
-cpi r11,3
-breq fordward
-cpi r11,0
-ldi r11,2
-breq turnleft
-jmp turnright
-
-fordward:
-nop
-call pasoadel
-mov r6,r0
-call getval
-cpi r3,0
-breq GOBACK
-mov r4,r9
-mov r5,r10
-call getNxy
-call getval
-cpi r3,1
-breq deducir
-jmp forward
-
-reverse:
-nop
-call pasoatra
-mov r6,r0
-call getval
-cpi r3,0
-breq GOBACK
-mov r4,r9
-mov r5,r10
-call getNxy
-call getval
-cpi r3,1
-breq deducir
-jmp reverse
-
-turnright:
-nop
-mov r3,r7
-jmp turnr
-
-turnr:
-nop
-call pasoizq
-cpi r3,0
-breq fordward
-dec r3
-jmp turnr
-
-turnleft:
-nop
-mov r3,r7
-jmp turnl
-
-turnl:
-nop
-call pasoder
-cpi r3,0
-breq fordward
-dec r3
-jmp turnr
-;fin rutinas deducir
-
 GOBACK:
-nop
-cpi r8,1
+cpi r24,1
 breq backB
 jmp backA
 
-backB:
-nop
-jmp moverup
-in r22,pinf
-nop
-com r22
-SBRS r22,1
-jmp backB
-jmp gostop
+RutSel:
+cp r18,r26
+brlo moverdown
+brne moverup
+cp r17,r25
+brlo moverleft
+brne moverright
+mov r22,r16
+call getval
+cpi r19,0
+breq GOBACK
+jmp fordward
 
-backA:
-nop
-jmp moverdown
-in r22,pinf
-nop
-com r22
-SBRS r22,0
-jmp backA
-jmp gostop
+turnright:
+call pasoizq
+jmp fordward
+
+moverdown:
+dec r26
+cpi r27,0
+breq reverse
+cpi r27,1
+breq fordward
+cpi r27,2
+ldi r27,1
+breq turnleft
+jmp turnright
+
+reverse:
+call pasoatra
+mov r22,r16
+call getval
+cpi r19,0
+breq GOBACK
+mov r20,r25
+mov r21,r26
+call getNxy
+call getval
+cpi r19,1
+breq deducir
+jmp reverse
+
+fordward:
+call pasoadel
+mov r22,r16
+call getval
+cpi r19,0
+breq GOBACK
+mov r20,r25
+mov r21,r26
+call getNxy
+call getval
+cpi r19,1
+breq deducir
+jmp fordward
+
+moverup:
+inc r26
+cpi r27,0
+breq fordward
+cpi r27,1
+breq reverse
+cpi r27,2
+ldi r27,0
+breq turnright
+jmp turnleft
+
+deducir:
+mov r22,r16
+call getval
+ldi r20,0
+cpse r19,r20
+jmp RutSel
+jmp GOBACK
+
+
+
+
+
+
+
+
+
+
+
+
+
+;fin rutinas deducir
 
 gostop:
 nop
@@ -557,7 +538,16 @@ jmp gostop
 
 
 
+stindi:
+jmp stindi
 
+pasoizq:
+
+pasoder:
+
+pasoatra:
+
+pasoadel:
 
 
 
@@ -567,14 +557,14 @@ jmp gostop
 stindi:
 nop
 call pasoadel
-in r22, pinf
+in r4, pinf
 nop
-com r22
+com r4
 nop
-cpi r22,0
+cpi r4,0
 breq stindi
 ;suponiendo que el borde del sector A entra por el bit 0 y que el del b entra por el bit 1...
-cpi r22,2
+cpi r4,2
 breq setsecA
 jmp setsecB
 
@@ -584,81 +574,81 @@ jmp setsecB
 stindiA:
 nop
 call pasoadel
-in r20, pinc
+in r2, pinc
 nop
-com r20
-cpi r20,0
+com r2
+cpi r2,0
 breq stindiA
-ldi r6,0
-SBRC r20,0
+ldi r22,0
+SBRC r2,0
 jmp stindiAA
-inc r6
-SBRC r20,1
+inc r22
+SBRC r2,1
 jmp stindiAA
-inc r6
-SBRC r20,2
+inc r22
+SBRC r2,2
 jmp stindiAA
-inc r6
-SBRC r20,3
+inc r22
+SBRC r2,3
 jmp stindiAA
-inc r6
-SBRC r20,4
+inc r22
+SBRC r2,4
 jmp stindiAA
-inc r6
-SBRC r20,5
+inc r22
+SBRC r2,5
 jmp stindiAA
-inc r6
-SBRC r20,6
+inc r22
+SBRC r2,6
 jmp stindiAA
-inc r6
+inc r22
 jmp stindiAA
 
 stindiAA:
 nop
 call getxy
-mov r9,r4
-mov r10,r5
+mov r25,r20
+mov r26,r21
 jmp deducir
 
 stindiB:
 nop
 call pasoadel
-in r21, pind
+in r3, pind
 nop
-com r21
-cpi r21,0
+com r3
+cpi r3,0
 breq stindiB
-ldi r6,0
-SBRC r21,0
+ldi r22,0
+SBRC r3,0
 jmp stindiBB
-inc r6
-SBRC r21,1
+inc r22
+SBRC r3,1
 jmp stindiBB
-inc r6
-SBRC r21,2
+inc r22
+SBRC r3,2
 jmp stindiBB
-inc r6
-SBRC r21,3
+inc r22
+SBRC r3,3
 jmp stindiBB
-inc r6
-SBRC r21,4
+inc r22
+SBRC r3,4
 jmp stindiBB
-inc r6
-SBRC r21,5
+inc r22
+SBRC r3,5
 jmp stindiBB
-inc r6
-SBRC r21,6
+inc r22
+SBRC r3,6
 jmp stindiBB
-inc r6
+inc r22
 jmp stindiBB
 
 stindiBB:
 nop
-ldi r4,8
-add r6,r4
+ldi r20,8
+add r22,r20
 call getxy
-mov r9,r4
-mov r10,r5
+mov r25,r20
+mov r26,r21
 jmp deducir
 ;fin
 
